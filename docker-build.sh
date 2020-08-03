8a26fde95edb817676fc8d36e5460f0d84f6bd86
Longze Chen <cslzchen@gmail.com>
2020-08-02 20:36:39 -0400

    Upgrade to 6.2.x

--- docker-build.sh
+++ docker-build.sh
@@ -2,9 +2,9 @@
 
 image_tag=(`cat gradle.properties | grep "cas.version" | cut -d= -f2`)
 
-echo "Building CAS docker image tagged as [$image_tag]"
+echo "Building CAS docker image tagged as [v$image_tag]"
 # read -p "Press [Enter] to continue..." any_key;
 
-docker build --tag="org.apereo.cas/cas:$image_tag" . \
-  && echo "Built CAS image successfully tagged as org.apereo.cas/cas:$image_tag" \
-  && docker images "org.apereo.cas/cas:$image_tag"
+docker build --tag="apereo/cas:v$image_tag" . \
+  && echo "Built CAS image successfully tagged as apereo/cas:v$image_tag" \
+  && docker images "apereo/cas:v$image_tag"
--- docker-push.sh
+++ docker-push.sh
@@ -7,6 +7,6 @@ echo "$docker_psw" | docker login --username "$docker_user" --password-stdin
 
 image_tag=(`cat gradle.properties | grep "cas.version" | cut -d= -f2`)
 
-echo "Pushing CAS docker image tagged as $image_tag to org.apereo.cas/cas..."
-docker push org.apereo.cas/cas:"$image_tag" \
-	&& echo "Pushed org.apereo.cas/cas:$image_tag successfully.";
+echo "Pushing CAS docker image tagged as v$image_tag to apereo/cas..."
+docker push apereo/cas:"v$image_tag" \
+	&& echo "Pushed apereo/cas:v$image_tag successfully.";
