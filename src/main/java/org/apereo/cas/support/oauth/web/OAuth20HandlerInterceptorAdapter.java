0285bcaa4d3bb968c56023bf7ce4c77e0b9ce3fd
Longze Chen <cslzchen@gmail.com>
2021-03-03 01:30:40 -0500

    Fix OAuth 2.0 intercepter to support OSF appetite

--- src/main/java/org/apereo/cas/support/oauth/web/OAuth20HandlerInterceptorAdapter.java
+++ src/main/java/org/apereo/cas/support/oauth/web/OAuth20HandlerInterceptorAdapter.java
@@ -3,17 +3,21 @@ package org.apereo.cas.support.oauth.web;
 import org.apereo.cas.services.ServicesManager;
 import org.apereo.cas.support.oauth.OAuth20Constants;
 import org.apereo.cas.support.oauth.OAuth20ResponseTypes;
-import org.apereo.cas.support.oauth.util.OAuth20Utils;
 import org.apereo.cas.support.oauth.web.response.accesstoken.ext.AccessTokenGrantRequestExtractor;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import lombok.val;
+
+import org.jetbrains.annotations.NotNull;
 import org.pac4j.core.context.JEEContext;
 import org.pac4j.core.context.session.SessionStore;
+
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import java.util.Collection;
 import java.util.Optional;
 import java.util.regex.Pattern;
@@ -21,11 +25,20 @@ import java.util.regex.Pattern;
 /**
  * This is {@link OAuth20HandlerInterceptorAdapter}.
  *
+ * This interceptor has been customized for OSF so that certain endpoints no longer required authentication pre-handle.
+ *
+ * {@code /oauth2/revoke} - OSF allows revoke one token with just the token to revoke. Thus checking client ID for authentication
+ *                         pre-handle breaks the endpoint.
+ * {@code /oauth2/device} - Disabled endpoints no longer needs extra pre-handle; the same applies to {@code /oauth2/introspect}.
+ *
  * @author Misagh Moayyed
+ * @author Longze Chen
  * @since 5.1.0
  */
 @RequiredArgsConstructor
+@Slf4j
 public class OAuth20HandlerInterceptorAdapter extends HandlerInterceptorAdapter {
+
     /**
      * Access token interceptor.
      */
@@ -43,48 +56,42 @@ public class OAuth20HandlerInterceptorAdapter extends HandlerInterceptorAdapter 
     private final SessionStore<JEEContext> sessionStore;
 
     @Override
-    public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response,
-                             final Object handler) throws Exception {
-        if (requestRequiresAuthentication(request, response)) {
+    public boolean preHandle(
+            @NotNull final HttpServletRequest request,
+            @NotNull final HttpServletResponse response,
+            @NotNull final Object handler
+    ) throws Exception {
+
+        if (isRevokeTokenRequest(request) || isDisabledEndpoints(request)) {
+            return true;
+        }
+        if (requestRequiresAuthentication(request)) {
             return requiresAuthenticationAccessTokenInterceptor.preHandle(request, response, handler);
         }
-
-        if (isDeviceTokenRequest(request, response)) {
+        if (isAuthorizationRequest(request)) {
             return requiresAuthenticationAuthorizeInterceptor.preHandle(request, response, handler);
         }
-
-        return !isAuthorizationRequest(request, response) || requiresAuthenticationAuthorizeInterceptor.preHandle(request, response, handler);
+        return true;
     }
 
     /**
-    * Is the client requesting is a OAuth "public" client?
-    * An OAuth "public" client is one that does not define a secret like a mobile application.
-    *
-    * @param request the request
-    * @param response the response
-    * @return true/false
-    */
-    protected boolean clientNeedAuthentication(final HttpServletRequest request, final HttpServletResponse response) {
-        val clientId = OAuth20Utils.getClientIdAndClientSecret(new JEEContext(request, response, sessionStore)).getLeft();
-        if (clientId.isEmpty()) {
-            return true;
-        }
-
-        val registeredService = OAuth20Utils.getRegisteredOAuthServiceByClientId(servicesManager, clientId);
-        if (registeredService == null) {
-            return true;
-        }
-        return OAuth20Utils.doesServiceNeedAuthentication(registeredService);
+     * Is authorization request.
+     *
+     * @param request  the request
+     * @return true/false
+     */
+    protected boolean isAuthorizationRequest(final HttpServletRequest request) {
+        val requestPath = request.getRequestURI();
+        return doesUriMatchPattern(requestPath, OAuth20Constants.AUTHORIZE_URL);
     }
 
     /**
      * Is a revoke token request?
      *
      * @param request  the request
-     * @param response the response
      * @return true/false
      */
-    protected boolean isRevokeTokenRequest(final HttpServletRequest request, final HttpServletResponse response) {
+    protected boolean isRevokeTokenRequest(final HttpServletRequest request) {
         val requestPath = request.getRequestURI();
         return doesUriMatchPattern(requestPath, OAuth20Constants.REVOCATION_URL);
     }
@@ -93,10 +100,9 @@ public class OAuth20HandlerInterceptorAdapter extends HandlerInterceptorAdapter 
      * Is access token request request.
      *
      * @param request  the request
-     * @param response the response
      * @return true/false
      */
-    protected boolean isAccessTokenRequest(final HttpServletRequest request, final HttpServletResponse response) {
+    protected boolean isAccessTokenRequest(final HttpServletRequest request) {
         val requestPath = request.getRequestURI();
         val pattern = String.format("(%s|%s)", OAuth20Constants.ACCESS_TOKEN_URL, OAuth20Constants.TOKEN_URL);
         return doesUriMatchPattern(requestPath, pattern);
@@ -106,30 +112,45 @@ public class OAuth20HandlerInterceptorAdapter extends HandlerInterceptorAdapter 
      * Is device token request boolean.
      *
      * @param request  the request
-     * @param response the response
      * @return true/false
      */
-    protected boolean isDeviceTokenRequest(final HttpServletRequest request, final HttpServletResponse response) {
+    protected boolean isDeviceTokenRequest(final HttpServletRequest request) {
         val requestPath = request.getRequestURI();
         val pattern = String.format("(%s)", OAuth20Constants.DEVICE_AUTHZ_URL);
         return doesUriMatchPattern(requestPath, pattern);
     }
 
     /**
+     * Is introspect request boolean.
+     *
+     * @param request  the request
+     * @return true/false
+     */
+    protected boolean isIntrospectRequest(final HttpServletRequest request) {
+        val requestPath = request.getRequestURI();
+        val pattern = String.format("(%s)", OAuth20Constants.INTROSPECTION_URL);
+        return doesUriMatchPattern(requestPath, pattern);
+    }
+
+    /**
+     * Is disabled endpoints request.
+     *
+     * @param request  the request
+     * @return true/false
+     */
+    protected boolean isDisabledEndpoints(final HttpServletRequest request) {
+        return isDeviceTokenRequest(request) || isIntrospectRequest(request);
+    }
+
+    /**
      * Request requires authentication.
      *
      * @param request  the request
-     * @param response the response
      * @return true/false
      */
-    protected boolean requestRequiresAuthentication(final HttpServletRequest request, final HttpServletResponse response) {
-        val revokeTokenRequest = isRevokeTokenRequest(request, response);
+    protected boolean requestRequiresAuthentication(final HttpServletRequest request) {
 
-        if (revokeTokenRequest) {
-            return clientNeedAuthentication(request, response);
-        }
-
-        val accessTokenRequest = isAccessTokenRequest(request, response);
+        val accessTokenRequest = isAccessTokenRequest(request);
         if (!accessTokenRequest) {
             val extractor = extractAccessTokenGrantRequest(request);
             if (extractor.isPresent()) {
@@ -151,18 +172,6 @@ public class OAuth20HandlerInterceptorAdapter extends HandlerInterceptorAdapter 
             .stream()
             .filter(ext -> ext.supports(request))
             .findFirst();
-    }
-
-    /**
-     * Is authorization request.
-     *
-     * @param request  the request
-     * @param response the response
-     * @return true/false
-     */
-    protected boolean isAuthorizationRequest(final HttpServletRequest request, final HttpServletResponse response) {
-        val requestPath = request.getRequestURI();
-        return doesUriMatchPattern(requestPath, OAuth20Constants.AUTHORIZE_URL);
     }
 
     /**
