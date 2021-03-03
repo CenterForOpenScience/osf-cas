d33d29b00b136b3b1335d79932c27bb132ff0d03
Longze Chen <cslzchen@gmail.com>
2021-03-03 01:21:51 -0500

    Disable device code and introspection endpoint (not used by OSF)
    Both "/device" and "/introspect" endpoints are not used by OSF. Thus
    disable both (including their trailing slash variants) by returning
    either the 501 NOT IMPLEMENTED JSON response or the OAuth error page
    when appropriate. In addition, relevant overlaid templates have been
    removed since they are no longer used.

--- src/main/java/org/apereo/cas/support/oauth/web/endpoints/OAuth20DeviceUserCodeApprovalEndpointController.java
+++ src/main/java/org/apereo/cas/support/oauth/web/endpoints/OAuth20DeviceUserCodeApprovalEndpointController.java
@@ -1,10 +1,14 @@
 package org.apereo.cas.support.oauth.web.endpoints;
 
 import org.apereo.cas.support.oauth.OAuth20Constants;
-import org.apereo.cas.ticket.device.OAuth20DeviceUserCode;
 
+import io.cos.cas.oauth.support.OsfCasOAuth20Constants;
+import io.cos.cas.oauth.support.OsfCasOAuth20ModelContext;
+import io.cos.cas.oauth.support.OsfCasOAuth20Utils;
+
+import lombok.extern.slf4j.Slf4j;
 import lombok.val;
-import org.apache.commons.lang3.StringUtils;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,20 +16,18 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 /**
  * This is {@link OAuth20DeviceUserCodeApprovalEndpointController}.
  *
+ * This controller is disabled for OSF CAS and returns authorization error page for GET or 501 NOT IMPLEMENTED for POST.
+ *
  * @author Misagh Moayyed
+ * @author Longze Chen
  * @since 6.0.0
  */
+@Slf4j
 public class OAuth20DeviceUserCodeApprovalEndpointController extends BaseOAuth20Controller {
-    /**
-     * User code parameter name.
-     */
-    public static final String PARAMETER_USER_CODE = "usercode";
 
     public OAuth20DeviceUserCodeApprovalEndpointController(final OAuth20ConfigurationContext oAuthConfigurationContext) {
         super(oAuthConfigurationContext);
@@ -38,10 +40,18 @@ public class OAuth20DeviceUserCodeApprovalEndpointController extends BaseOAuth20
      * @param response the response
      * @return the model and view
      */
-    @GetMapping(path = OAuth20Constants.BASE_OAUTH20_URL + '/' + OAuth20Constants.DEVICE_AUTHZ_URL)
-    public static ModelAndView handleGetRequest(final HttpServletRequest request, final HttpServletResponse response) {
-        val model = getApprovalModel(StringUtils.EMPTY);
-        return new ModelAndView(OAuth20Constants.DEVICE_CODE_APPROVAL_VIEW, model);
+    @GetMapping(path = {
+            OAuth20Constants.BASE_OAUTH20_URL + '/' + OAuth20Constants.DEVICE_AUTHZ_URL,
+            OAuth20Constants.BASE_OAUTH20_URL + '/' + OAuth20Constants.DEVICE_AUTHZ_URL + '/'
+    })
+    public ModelAndView handleGetRequest(final HttpServletRequest request, final HttpServletResponse response) {
+        val modelContext = new OsfCasOAuth20ModelContext();
+        modelContext.setErrorCode(OsfCasOAuth20Constants.NOT_IMPLEMENTED);
+        modelContext.setErrorParam(OAuth20Constants.BASE_OAUTH20_URL + "/" + OAuth20Constants.DEVICE_AUTHZ_URL);
+        modelContext.setErrorMsg(String.format("GET %s has not been implemented.", modelContext.getErrorParam()));
+        modelContext.setOsfUrl(getOsfUrl());
+        LOGGER.error(modelContext.getErrorMsg());
+        return OsfCasOAuth20Utils.produceOAuth20ErrorView(modelContext);
     }
 
     /**
@@ -51,39 +61,12 @@ public class OAuth20DeviceUserCodeApprovalEndpointController extends BaseOAuth20
      * @param response the response
      * @return the model and view
      */
-    @PostMapping(path = OAuth20Constants.BASE_OAUTH20_URL + '/' + OAuth20Constants.DEVICE_AUTHZ_URL)
+    @PostMapping(path = {
+            OAuth20Constants.BASE_OAUTH20_URL + '/' + OAuth20Constants.DEVICE_AUTHZ_URL,
+            OAuth20Constants.BASE_OAUTH20_URL + '/' + OAuth20Constants.DEVICE_AUTHZ_URL + '/'
+    })
     public ModelAndView handlePostRequest(final HttpServletRequest request, final HttpServletResponse response) {
-        val userCode = request.getParameter(PARAMETER_USER_CODE);
-        val codeNotfound = getModelAndViewForFailure("codenotfound");
-        if (StringUtils.isBlank(userCode)) {
-            return codeNotfound;
-        }
-        val codeId = getOAuthConfigurationContext().getDeviceTokenFactory().generateDeviceUserCode(userCode);
-        val deviceUserCode = getOAuthConfigurationContext().getTicketRegistry().getTicket(codeId, OAuth20DeviceUserCode.class);
-        if (deviceUserCode == null) {
-            return codeNotfound;
-        }
-        if (deviceUserCode.isExpired()) {
-            return getModelAndViewForFailure("codeexpired");
-        }
-        if (deviceUserCode.isUserCodeApproved()) {
-            return getModelAndViewForFailure("codeapproved");
-        }
-        deviceUserCode.approveUserCode();
-        getOAuthConfigurationContext().getTicketRegistry().updateTicket(deviceUserCode);
-        return new ModelAndView(OAuth20Constants.DEVICE_CODE_APPROVED_VIEW, HttpStatus.OK);
-    }
-
-    private static ModelAndView getModelAndViewForFailure(final String code) {
-        return new ModelAndView(OAuth20Constants.DEVICE_CODE_APPROVAL_VIEW, getApprovalModel(code));
-    }
-
-    private static Map getApprovalModel(final String errorCode) {
-        val map = new LinkedHashMap<String, Object>();
-        map.put("prefix", OAuth20DeviceUserCode.PREFIX);
-        if (StringUtils.isNotBlank(errorCode)) {
-            map.put("error", errorCode);
-        }
-        return map;
+        LOGGER.error("POST {}/{} has not been implemented.", OAuth20Constants.BASE_OAUTH20_URL, OAuth20Constants.DEVICE_AUTHZ_URL);
+        return OsfCasOAuth20Utils.writeError(response, OsfCasOAuth20Constants.NOT_IMPLEMENTED, HttpStatus.NOT_IMPLEMENTED);
     }
 }
--- src/main/java/org/apereo/cas/support/oauth/web/endpoints/OAuth20IntrospectionEndpointController.java
+++ src/main/java/org/apereo/cas/support/oauth/web/endpoints/OAuth20IntrospectionEndpointController.java
@@ -1,23 +1,14 @@
 package org.apereo.cas.support.oauth.web.endpoints;
 
-import org.apereo.cas.audit.AuditableContext;
-import org.apereo.cas.authentication.AuthenticationManager;
 import org.apereo.cas.support.oauth.OAuth20Constants;
-import org.apereo.cas.support.oauth.services.OAuthRegisteredService;
 import org.apereo.cas.support.oauth.util.OAuth20Utils;
 import org.apereo.cas.support.oauth.web.response.introspection.OAuth20IntrospectionAccessTokenResponse;
-import org.apereo.cas.ticket.InvalidTicketException;
-import org.apereo.cas.ticket.accesstoken.OAuth20AccessToken;
-import org.apereo.cas.util.CollectionUtils;
-import org.apereo.cas.util.HttpRequestUtils;
+
+import io.cos.cas.oauth.support.OsfCasOAuth20Constants;
 
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
-import org.apache.commons.lang3.StringUtils;
-import org.pac4j.core.context.JEEContext;
-import org.pac4j.core.credentials.UsernamePasswordCredentials;
-import org.pac4j.core.credentials.extractor.BasicAuthExtractor;
-import org.springframework.http.HttpHeaders;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -28,14 +19,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.util.ArrayList;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
 /**
  * This is {@link OAuth20IntrospectionEndpointController}.
  *
+ * This controller is disabled for OSF CAS and returns 501 NOT IMPLEMENTED for both GET and POST.
+ *
  * @author Misagh Moayyed
+ * @author Longze Chen
  * @since 6.0.0
  */
 @Slf4j
@@ -46,36 +36,16 @@ public class OAuth20IntrospectionEndpointController extends BaseOAuth20Controlle
     }
 
     /**
-     * Build unauthorized response entity.
+     * Build not implemented response entity.
      *
-     * @param code the code
      * @return the response entity
      */
-    private static ResponseEntity<OAuth20IntrospectionAccessTokenResponse> buildUnauthorizedResponseEntity(final String code,
-                                                                                                           final boolean isAuthenticationFailure) {
+    private static ResponseEntity<OAuth20IntrospectionAccessTokenResponse> buildNotImplementedResponseEntity() {
         val map = new LinkedMultiValueMap<String, String>(1);
-        map.add(OAuth20Constants.ERROR, code);
+        map.add(OAuth20Constants.ERROR, OsfCasOAuth20Constants.NOT_IMPLEMENTED);
         val value = OAuth20Utils.toJson(map);
         val headers = new LinkedMultiValueMap<String, String>();
-        if (isAuthenticationFailure) {
-            headers.add(HttpHeaders.WWW_AUTHENTICATE, "Basic");
-        }
-        val result = (ResponseEntity<OAuth20IntrospectionAccessTokenResponse>) new ResponseEntity(value, headers, HttpStatus.UNAUTHORIZED);
-        return result;
-    }
-
-    /**
-     * Build bad request response entity.
-     *
-     * @param code the code
-     * @return the response entity
-     */
-    private static ResponseEntity<OAuth20IntrospectionAccessTokenResponse> buildBadRequestResponseEntity(final String code) {
-        val map = new LinkedMultiValueMap<String, String>(1);
-        map.add(OAuth20Constants.ERROR, code);
-        val value = OAuth20Utils.toJson(map);
-        val result = (ResponseEntity<OAuth20IntrospectionAccessTokenResponse>) new ResponseEntity(value, HttpStatus.BAD_REQUEST);
-        return result;
+        return new ResponseEntity(value, headers, HttpStatus.NOT_IMPLEMENTED);
     }
 
     /**
@@ -85,12 +55,19 @@ public class OAuth20IntrospectionEndpointController extends BaseOAuth20Controlle
      * @param response the response
      * @return the response entity
      */
-    @GetMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
-        produces = MediaType.APPLICATION_JSON_VALUE,
-        value = '/' + OAuth20Constants.BASE_OAUTH20_URL + '/' + OAuth20Constants.INTROSPECTION_URL)
-    public ResponseEntity<OAuth20IntrospectionAccessTokenResponse> handleRequest(final HttpServletRequest request,
-                                                                                 final HttpServletResponse response) {
-        return handlePostRequest(request, response);
+    @GetMapping(
+            value = {
+                    '/' + OAuth20Constants.BASE_OAUTH20_URL + '/' + OAuth20Constants.INTROSPECTION_URL,
+                    '/' + OAuth20Constants.BASE_OAUTH20_URL + '/' + OAuth20Constants.INTROSPECTION_URL + '/'
+            },
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<OAuth20IntrospectionAccessTokenResponse> handleRequest(
+            final HttpServletRequest request,
+            final HttpServletResponse response
+    ) {
+        LOGGER.error("GET {}/{} has not been implemented.", OAuth20Constants.BASE_OAUTH20_URL, OAuth20Constants.INTROSPECTION_URL);
+        return buildNotImplementedResponseEntity();
     }
 
     /**
@@ -100,122 +77,18 @@ public class OAuth20IntrospectionEndpointController extends BaseOAuth20Controlle
      * @param response the response
      * @return the response entity
      */
-    @PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
-        produces = MediaType.APPLICATION_JSON_VALUE,
-        value = '/' + OAuth20Constants.BASE_OAUTH20_URL + '/' + OAuth20Constants.INTROSPECTION_URL)
-    public ResponseEntity<OAuth20IntrospectionAccessTokenResponse> handlePostRequest(final HttpServletRequest request,
-                                                                                     final HttpServletResponse response) {
-        ResponseEntity<OAuth20IntrospectionAccessTokenResponse> result;
-        try {
-            val authExtractor = new BasicAuthExtractor();
-
-            val context = new JEEContext(request, response, getOAuthConfigurationContext().getSessionStore());
-            val credentialsResult = authExtractor.extract(context);
-
-            if (credentialsResult.isEmpty()) {
-                return buildUnauthorizedResponseEntity(OAuth20Constants.INVALID_CLIENT, true);
-            }
-
-            val credentials = credentialsResult.get();
-            val service = OAuth20Utils.getRegisteredOAuthServiceByClientId(
-                getOAuthConfigurationContext().getServicesManager(), credentials.getUsername());
-            val validationError = validateIntrospectionRequest(service, credentials, request);
-            if (validationError.isPresent()) {
-                result = validationError.get();
-            } else {
-                val accessToken = StringUtils.defaultIfBlank(request.getParameter(OAuth20Constants.TOKEN),
-                    request.getParameter(OAuth20Constants.ACCESS_TOKEN));
-
-                LOGGER.debug("Located access token [{}] in the request", accessToken);
-                var ticket = (OAuth20AccessToken) null;
-                try {
-                    val token = extractAccessTokenFrom(accessToken);
-                    ticket = getOAuthConfigurationContext().getCentralAuthenticationService().getTicket(token, OAuth20AccessToken.class);
-                } catch (final InvalidTicketException e) {
-                    LOGGER.trace(e.getMessage(), e);
-                    LOGGER.info("Unable to fetch access token [{}]: [{}]", accessToken, e.getMessage());
-                }
-
-                if (service == null) {
-                    LOGGER.error("Unable to determine service");
-                    return buildUnauthorizedResponseEntity(OAuth20Constants.INVALID_CLIENT, true);
-                }
-
-                val introspect = createIntrospectionValidResponse(service, ticket);
-                result = new ResponseEntity<>(introspect, HttpStatus.OK);
-            }
-        } catch (final Exception e) {
-            LOGGER.error(e.getMessage(), e);
-            result = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-        return result;
-    }
-
-    private Optional<ResponseEntity<OAuth20IntrospectionAccessTokenResponse>> validateIntrospectionRequest(final OAuthRegisteredService registeredService,
-                                                                                                           final UsernamePasswordCredentials credentials,
-                                                                                                           final HttpServletRequest request) {
-        val tokenExists = HttpRequestUtils.doesParameterExist(request, OAuth20Constants.TOKEN)
-            || HttpRequestUtils.doesParameterExist(request, OAuth20Constants.ACCESS_TOKEN);
-
-        if (!tokenExists) {
-            return Optional.of(buildBadRequestResponseEntity(OAuth20Constants.MISSING_ACCESS_TOKEN));
-        }
-
-        if (OAuth20Utils.checkClientSecret(registeredService, credentials.getPassword(),
-            getOAuthConfigurationContext().getRegisteredServiceCipherExecutor())) {
-            val service = getOAuthConfigurationContext().getWebApplicationServiceServiceFactory().createService(registeredService.getServiceId());
-            val audit = AuditableContext.builder()
-                .service(service)
-                .registeredService(registeredService)
-                .build();
-            val accessResult = getOAuthConfigurationContext().getRegisteredServiceAccessStrategyEnforcer().execute(audit);
-            return accessResult.isExecutionFailure()
-                ? Optional.of(buildUnauthorizedResponseEntity(OAuth20Constants.UNAUTHORIZED_CLIENT, false))
-                : Optional.empty();
-        }
-        return Optional.of(buildUnauthorizedResponseEntity(OAuth20Constants.INVALID_CLIENT, true));
-    }
-
-    /**
-     * Create introspection response OAuth introspection access token response.
-     *
-     * @param service the service
-     * @param ticket  the ticket
-     * @return the OAuth introspection access token response
-     */
-    protected OAuth20IntrospectionAccessTokenResponse createIntrospectionValidResponse(final OAuthRegisteredService service,
-                                                                                       final OAuth20AccessToken ticket) {
-        val introspect = new OAuth20IntrospectionAccessTokenResponse();
-        introspect.setClientId(service.getClientId());
-        introspect.setScope("CAS");
-        introspect.setAud(service.getServiceId());
-        introspect.setIss(getOAuthConfigurationContext().getCasProperties().getAuthn().getOidc().getIssuer());
-
-        if (ticket != null) {
-            introspect.setActive(true);
-            val authentication = ticket.getAuthentication();
-            val subject = authentication.getPrincipal().getId();
-            introspect.setSub(subject);
-            introspect.setUniqueSecurityName(subject);
-            introspect.setIat(ticket.getCreationTime().toInstant().getEpochSecond());
-            introspect.setExp(introspect.getIat() + ticket.getExpirationPolicy().getTimeToLive());
-
-            val methods = authentication.getAttributes().get(AuthenticationManager.AUTHENTICATION_METHOD_ATTRIBUTE);
-            val realmNames = CollectionUtils.toCollection(methods)
-                .stream()
-                .map(Object::toString)
-                .collect(Collectors.joining(","));
-
-            introspect.setRealmName(realmNames);
-            introspect.setTokenType(OAuth20Constants.TOKEN_TYPE_BEARER);
-
-            val grant = authentication.getAttributes().getOrDefault(OAuth20Constants.GRANT_TYPE, new ArrayList<>(0));
-            if (!grant.isEmpty()) {
-                introspect.setGrantType(grant.get(0).toString().toLowerCase());
-            }
-        } else {
-            introspect.setActive(false);
-        }
-        return introspect;
+    @PostMapping(
+            value = {
+                    '/' + OAuth20Constants.BASE_OAUTH20_URL + '/' + OAuth20Constants.INTROSPECTION_URL,
+                    '/' + OAuth20Constants.BASE_OAUTH20_URL + '/' + OAuth20Constants.INTROSPECTION_URL + '/'
+            },
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<OAuth20IntrospectionAccessTokenResponse> handlePostRequest(
+            final HttpServletRequest request,
+            final HttpServletResponse response
+    ) {
+        LOGGER.error("POST {}/{} has not been implemented.", OAuth20Constants.BASE_OAUTH20_URL, OAuth20Constants.INTROSPECTION_URL);
+        return buildNotImplementedResponseEntity();
     }
 }
